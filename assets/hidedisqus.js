4e757db425f978e1dd1a8909dbbf5eca587ae989
buileduy <binhbet+01@gmail.com>
2017-04-26 20:32:20 +0700

    comment

---
+++ assets/hidedisqus.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.show-comments').on('click', function(){
+          var disqus_shortname = 'thichdoc'; // Replace this value with *your* username.
+
+          // ajax request to load the disqus javascript
+          $.ajax({
+                  type: "GET",
+                  url: "http://" + disqus_shortname + ".disqus.com/embed.js",
+                  dataType: "script",
+                  cache: true
+          });
+          // hide the button once comments load
+          $(this).fadeOut();
+    });
+});
